e6f53046ce0a54d759ae47ca990f91fde91c5bd5
Gbenro Selere <seleregb@gmail.com>
2018-05-05 16:52:13 -0300

    added routing feature

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+// import {StepsModule} from 'primeng/steps';
+// import {MenuItem} from 'primeng/api';
+import { LoggerService } from '../../services/logger.service';
 
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
-})
+@Component({selector: 'app-home', templateUrl: './home.component.html', styleUrls: ['./home.component.css']})
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // items : MenuItem[];
+  activeIndex: number = 0;
+
+  constructor(private logger: LoggerService) {}
 
   ngOnInit() {
+    
+  }
+
+  handleChange(event) {
+    this.logger.log(`clicked step ${event}`);
   }
 
 }
